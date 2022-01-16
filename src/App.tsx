4168bd914f1cb7c601609f26e5266854f7e054b6
Oleg <shevchenko.oleg.87@gmail.com>
2022-01-16 15:34:55 +0300

    08_lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Technologies from "./Technologies";
+import Header from "./Header";
 
 function App() {
   return (
     <div>
+        <Header />
         <Technologies />
     </div>
   );
