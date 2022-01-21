5e60802e4f873b5a7371ad7170fd38b85c69ba38
Oleg <shevchenko.oleg.87@gmail.com>
2022-01-21 16:08:07 +0300

    module Dialogs added

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Footer from "./components/Footer/Footer";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className='app-wrapper'>
             <Header />
             <Navbar />
-            <Profile />
+            <div className={style.content}></div>
+            {/*<Profile />*/}
+            <Dialogs />
             <Footer />
         </div>
     );
